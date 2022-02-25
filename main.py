3d708efed0806896dd90e6b699b20de60351fb45
EmmaPd <91660057+EmmaPd@users.noreply.github.com>
2022-02-25 13:05:28 +0200

    updated title

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from flask_gravatar import Gravatar
 from forms import RegisterForm, CreatePostForm, LoginForm, CommentForm
 from functools import wraps
 
+#gunicorn==20.1.0
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
